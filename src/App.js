730d14fb99b490353b5d21a496bff196dfbf4204
Alan Nguyen <nguyenalan@berkeley.edu>
2019-07-18 17:49:57 -0400

    stateless components/app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Texture from './components/Texture'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default function App() {
+const App = () => {
   return (
     <div className="main">
       <div className="splash">
-        <p>hello</p>
-        <p>test</p>
       </div>
       <div className="display">
         <Texture/>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default App;
--- src/components/Texture.js
+++ src/components/Texture.js
@@ -5,56 +5,47 @@ import { perlin } from '../shaders/perlin'
 import img from '../img/fritz.jpg'
 import disp from '../img/displacement/noise.png'
 
-class Texture extends Component {
+const InitImage = ({url, disp}) => {
+  const [texture, noise] = useMemo(
+    () => {
+      const loader = new THREE.TextureLoader()
+      return [loader.load(url), loader.load(disp)]
+    },
+    [url, disp]
+  );
+  const { canvas } = useThree();
+  canvas.height=900;
+  canvas.width=700;
+  canvas.style="width: 100%; height: 100%;";
+  return (
+    <mesh>
+      <planeBufferGeometry name="geometry" args={[8, 8]} />
+        <shaderMaterial
+          name="material"
+          args={[perlin]}
+          uniforms-texture-value={texture}
+          uniforms-disp-value={noise}
+        />
+    </mesh>
+  )
+};
 
-  constructor(props) {
-    super(props);
-    this.initImage = this.initImage.bind(this);
-    this.Scene = this.Scene.bind(this);
-  }
+const Scene = () => {
+  let group = useRef();
+  useRender(() => group.current.children[0].material.uniforms['time'].value += 0.02);
+  return (
+    <group ref={group}>
+      <InitImage url={img} disp={disp} />
+    </group>
+  );
+}
 
-  initImage({ url, disp }) {
-    const [texture, noise] = useMemo(
-      () => {
-        const loader = new THREE.TextureLoader()
-        return [loader.load(url), loader.load(disp)]
-      },
-      [url, disp]
-    );
-    const { canvas } = useThree();
-    canvas.height=900;
-    canvas.width=700;
-    canvas.style="width: 100%; height: 100%;";
-    return (
-      <mesh>
-        <planeBufferGeometry name="geometry" args={[8, 8]} />
-          <shaderMaterial
-            name="material"
-            args={[perlin]}
-            uniforms-texture-value={texture}
-            uniforms-disp-value={noise}
-          />
-      </mesh>
-    )
-  }
-
-  Scene() {
-    let group = useRef();
-    useRender(() => group.current.children[0].material.uniforms['time'].value += 0.02);
-    return (
-      <group ref={group}>
-        <this.initImage url={img} disp={disp} />
-      </group>
-    );
-  }
-
-  render() {
-    return (
-        <Canvas className="canvas">
-            <this.Scene />
-        </Canvas>
-    );
-  }
+const Texture = () => {
+  return (
+    <Canvas className="canvas">
+      <Scene />
+    </Canvas>
+  );
 }
 
 export default Texture;
