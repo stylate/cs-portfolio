aaadb26cd6462c01865e7ee7fc40efaf1ace2ba1
Alan Nguyen <nguyenalan@berkeley.edu>
2019-07-19 16:21:40 -0400

    tinkered with font/styling, drafted splash component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Text = styled.h2`
-    font-size: 32px;
-    font-family: 'Open Sans', sans-serif;
-    padding-left: 2em;
-    padding-top: 3em;
+    font-size: 48px;
+    font-family: 'EB Garamond', sans-serif;
+    padding-left: 1.3em;
+    padding-top: 5em;
+    opacity: 0.9;
+    font-weight: 100;
+    letter-spacing: 1.2px;
 `
 
 const Header = (
-    <Text>fdkjfjdsfjksdfkjsbjdkf</Text>
+    <Text>alan nguyen</Text>
 );
 
 export default Header;
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 // this is text for the splash page.
 
 const Text = styled.div`
-    font-weight: 200;
+    font-weight: 100;
     padding-left: 4em;
-    padding-top: 4em;
+    padding-top: 0em;
     width: 45%;
 `
 
+const Mini = styled.p`
+    font-size: 12px;
+    line-height: 0.7;
+`
+
+const blurb = "currently a fourth year student at UC Berkeley studying computer science. " +  
+"interests lie heavily within design, digital media, security, and computer graphics."
+
 const Splash = (
     <Text>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>hello2</p>
+        <p>{blurb}</p>
+        <Mini>previously >> akamai, fox networks</Mini>
+        <Mini>project directory >> here</Mini>
     </Text>
 );
 
