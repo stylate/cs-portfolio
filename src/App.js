d7b63f3dd5f81f95a2cb2a240b8d9bf1975da130
Andy Tran <andy@mixmax.com>
2019-07-30 23:37:18 -0700

    Added in suggestions.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react'
-import { Header, Splash, Texture } from './components/Index'
+import MediaQuery from 'react-responsive';
+
+import { Texture } from './components/Index'
 import './layout/styles.scss'
+
+import Layout from './components/Layout';
+import Splash from './components/Splash';
 
 const App = () => {
   return (
-    <div className="main">
-      <div className="splash">
-        {Header}
-        {Splash}
-      </div>
-      <div className="display">
-        <Texture/>
-      </div>
-    </div>
+    <Layout>
+      <Splash />
+      <MediaQuery minWidth={769}>
+        <Texture />
+      </MediaQuery>
+    </Layout>
   );
+
+  // return (
+  //   <div className="main">
+  //     <div className="splash">
+  //       {Header}
+  //       {Splash}
+  //     </div>
+  //     <div className="display">
+  //       <Texture/>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export default Layout;
